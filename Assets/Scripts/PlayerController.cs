f17762fd1e123a5892ec404e25345f83c1ba30e0
William <will@ministrycentered.com>
2014-01-25 15:09:20 -0800

    Movements for Ball
    Created ledges, building, and sorting layers for this

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float playerSpeed;
+
+	private Rigidbody2D controller;
+
+	void Start ()
+	{
+		controller = gameObject.GetComponent<Rigidbody2D> ();
+		
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		Vector2 movement = new Vector2 (moveHorizontal, 0.0f);
+
+		controller.AddForce (movement * playerSpeed * Time.deltaTime);
+	}
+	
+}
